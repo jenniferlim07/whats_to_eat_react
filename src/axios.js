63574a678cb329a6a27b11fe7815532683cc1f72
Jen Lim <jennifer.lim07@gmail.com>
2021-08-10 14:49:39 -0700

    remove timeout

--- src/axios.js
+++ src/axios.js
@@ -5,7 +5,7 @@ const baseURL = 'https://whatstoeat-backend.herokuapp.com/';
 
 const axiosInstance = axios.create({
 	baseURL: baseURL,
-	timeout: 5000,
+	// timeout: 5000,
 	headers: {
 		Authorization: localStorage.getItem('access_token')
 			? 'JWT ' + localStorage.getItem('access_token')
